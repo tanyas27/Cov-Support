c787d36735955af33e3697ab4a830696779363ed
Tanya Singh <tanya@Tanyas-MacBook-Air.local>
2021-05-05 20:28:55 +0530

    Copy details functionality added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,15 @@ import yes from '../../Utils/images/verification.png';
 import no from '../../Utils/images/no.png';
 
 function Card(props) {
+  const copyText = `
+  Resource Available: ${props.field} \n 
+  Name: ${props.name} \n
+  Phone: ${props.phone} \n
+  City: ${props.city} \n
+  Description: ${props.description} \n
+  For more such resource related information, 
+  visit: https://cov-support.web.app/find-help
+  `;
   return (
     <div className={classes.Card}>
       <div className={classes.head}>
@@ -15,6 +24,7 @@ function Card(props) {
         <span className={classes.title}>City:</span>{props.city}<br/>
         <span className={classes.title}>Verified:</span>{props.verified ? <img src={yes} alt="verified"/> : <img src={no} alt="not verified"/>}<br/>
         <span className={classes.title}>Source:</span> {props.source}
+        <button className={classes.copy} onClick={() => {navigator.clipboard.writeText(copyText)}}>Copy</button>
       </div>
     </div>
   );
